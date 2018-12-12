44885efe0801b4ed6552af661482f4181bf0ef52
Tim Jensen <tim.l.jensen@gmail.com>
2018-12-11 18:20:54 -0600

    Drop database after every test method so that each test starts with a clean slate.

--- tests/base_handler_test_case.py
+++ tests/base_handler_test_case.py
@@ -15,7 +15,7 @@ class ExampleHandler(RequestHandler):
 
 class BaseHandlerTestCase(AsyncHTTPTestCase):
     def get_app(self):
-        mongodb_uri = os.environ.get("MONGODB_URI", "mongodb://localhost/testing")
+        mongodb_uri = os.environ.get("MONGODB_URI", "mongodb://localhost/motor-test-example")
         self.motor_client = motor.MotorClient(mongodb_uri)
         self.database = self.motor_client.get_database()
 
@@ -24,6 +24,14 @@ class BaseHandlerTestCase(AsyncHTTPTestCase):
                 ("/handler", ExampleHandler)
             ],
             database=self.database)
+
+    async def async_teardown(self):
+        await self.motor_client.drop_database(self.database)
+
+    def tearDown(self):
+        self.io_loop.run_sync(self.async_teardown)
+        self.motor_client.close()
+        super().tearDown()
 
     async def async_fetch(self, path):
         return await AsyncHTTPClient().fetch(self.get_url(path), raise_error=False)
